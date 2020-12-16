e56492db7452661beb5e1c63924e696a71c05721
Dipendra-creator <66115431+Dipendra-creator@users.noreply.github.com>
2020-12-16 17:13:49 +0530

    📖 Updated Math ✅

--- Mathematics/Maths.py
+++ Mathematics/Maths.py
@@ -1,6 +1,6 @@
 def classificationOfSecondOrderPDE():
-    print(f"Formula ==>>  A (du^2/dx^2) + B (du^2/dx.dy) + C (du^2/dy^2)\n"
-          f"So You Have To Provide Values of A,B and C respectively\n\n")
+    print(f"\nFormula ==>>  A (du^2/dx^2) + B (du^2/dx.dy) + C (du^2/dy^2)\n"
+          f"So You Have To Provide Values of A,B and C respectively\n")
     A = int(input("Value of A: "))
     B = int(input("Value of B: "))
     C = int(input("Value of C: "))
@@ -9,11 +9,11 @@ def classificationOfSecondOrderPDE():
     condition = (B ** 2) - (4 * A * C)
 
     if condition < 0:
-        print(f"Elliptic Because {condition}<0")
+        print(f"\nElliptic Because {condition} < 0")
     elif condition > 0:
-        print(f"Hyperbolic Because {condition}>0")
+        print(f"\nHyperbolic Because {condition} > 0")
     else:
-        print(f"Parabolic Because {condition}=0")
+        print(f"\nParabolic Because {condition} = 0")
 
 
 classificationOfSecondOrderPDE()
